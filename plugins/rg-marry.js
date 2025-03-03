c1d4186bd485120d7d000a61dcb3eb89cfb52a76
Liu <ld.unknown.2001@gmail.com>
2025-03-03 12:35:03 -0500

    Update rg-marry.js

--- plugins/rg-marry.js
+++ plugins/rg-marry.js
@@ -1,75 +1,37 @@
-import fs from 'fs';
-import path from 'path';
-
-const marriagesFile = path.resolve('./src/database/casados.json');
-let proposals = {};
-
-function loadMarriages() {
-    if (fs.existsSync(marriagesFile)) {
-        const data = fs.readFileSync(marriagesFile, 'utf-8');
-        return JSON.parse(data);
-    } else return {};
-}
-
-function saveMarriages(data) {
-    fs.writeFileSync(marriagesFile, JSON.stringify(data, null, 2));
-}
-
-let marriages = loadMarriages();
-
-let handler = async (m, { conn, command, usedPrefix, args }) => {
+let handler = async (m, { conn, args }) => {
     let userId = m.sender;
     let mentionedUsers = m.mentionedJid || [];
 
-    if (command === 'marry') {
-        if (!mentionedUsers.length) {
-            return await conn.reply(m.chat, `✧ Debes mencionar a alguien para proponer matrimonio.\nEjemplo: *${usedPrefix}marry @usuario*`, m);
-        }
-
-        let partner = mentionedUsers[0];
-
-        if (marriages[userId]) {
-            return await conn.reply(m.chat, `✧ Ya estás casado/a con *@${marriages[userId].split('@')[0]}*\nUsa *${usedPrefix}divorce* para divorciarte.`, m, { mentions: [marriages[userId]] });
-        }
-
-        if (marriages[partner]) {
-            return await conn.reply(m.chat, `✧ @${partner.split('@')[0]} ya está casado/a con *@${marriages[partner].split('@')[0]}*`, m, { mentions: [partner, marriages[partner]] });
-        }
-
-        if (userId === partner) {
-            return await conn.reply(m.chat, '✧ ¡No puedes casarte contigo mismo/a!', m);
-        }
-
-        if (proposals[partner] && proposals[partner] === userId) {
-            delete proposals[partner];
-            marriages[userId] = partner;
-            marriages[partner] = userId;
-            saveMarriages(marriages);
-
-            return await conn.reply(m.chat, `🎉 ¡Felicidades! *@${userId.split('@')[0]}* y *@${partner.split('@')[0]}* se han casado. 💍`, m, { mentions: [userId, partner] });
-        } else {
-            proposals[userId] = partner;
-            return await conn.reply(m.chat, `💕 @${partner.split('@')[0]}, @${userId.split('@')[0]} te ha propuesto matrimonio. ¿Aceptas?\nResponde con *${usedPrefix}marry @${userId.split('@')[0]}* para aceptar.`, m, { mentions: [userId, partner] });
-        }
+    if (!mentionedUsers.length) {
+        return await conn.reply(m.chat, `✧ Debes mencionar a alguien para casarte.\nEjemplo: *#marry @usuario*`, m);
     }
 
-    if (command === 'divorce') {
-        if (!marriages[userId]) {
-            return await conn.reply(m.chat, '✧ No estás casado/a con nadie.', m);
-        }
+    let partnerId = mentionedUsers[0];
 
-        let exPartner = marriages[userId];
-        delete marriages[userId];
-        delete marriages[exPartner];
-        saveMarriages(marriages);
+    if (!global.db.data.users[userId]) global.db.data.users[userId] = {};
+    if (!global.db.data.users[partnerId]) global.db.data.users[partnerId] = {};
 
-        return await conn.reply(m.chat, `💔 @${userId.split('@')[0]} y @${exPartner.split('@')[0]} se han divorciado. 😢`, m, { mentions: [userId, exPartner] });
+    if (global.db.data.users[userId].marry) {
+        return await conn.reply(m.chat, `✧ Ya estás casado/a con *@${global.db.data.users[userId].marry.split('@')[0]}*.\nUsa *#divorce* para divorciarte.`, m, { mentions: [global.db.data.users[userId].marry] });
     }
+
+    if (global.db.data.users[partnerId].marry) {
+        return await conn.reply(m.chat, `✧ @${partnerId.split('@')[0]} ya está casado/a con *@${global.db.data.users[partnerId].marry.split('@')[0]}*`, m, { mentions: [partnerId, global.db.data.users[partnerId].marry] });
+    }
+
+    if (userId === partnerId) {
+        return await conn.reply(m.chat, '✧ ¡No puedes casarte contigo mismo/a!', m);
+    }
+
+    global.db.data.users[userId].marry = partnerId;
+    global.db.data.users[partnerId].marry = userId;
+
+    await conn.reply(m.chat, `💍 ¡Felicidades! *@${userId.split('@')[0]}* y *@${partnerId.split('@')[0]}* ahora están casados.`, m, { mentions: [userId, partnerId] });
 };
 
-handler.help = ['marry @usuario', 'divorce'];
+handler.help = ['marry @usuario'];
 handler.tags = ['fun'];
-handler.command = ['marry', 'divorce'];
+handler.command = ['marry'];
 handler.group = true;
 handler.register = true;
 
