ce2f9cd8cd7f68403909840f9c8c8a11fd5d76b6
Liu <ld.unknown.2001@gmail.com>
2025-03-03 12:47:23 -0500

    Update gacha-rollwaifu.js

--- plugins/gacha-rollwaifu.js
+++ plugins/gacha-rollwaifu.js
@@ -101,7 +101,7 @@ ID: *${randomCharacter.id}*`;
     }
 
     await saveCharacters(characters);
-    cooldowns[userId] = now + 15 * 60 * 1000;
+    cooldowns[userId] = now + 20 * 60 * 1000;
   } catch (error) {
     await conn.reply(m.chat, `✘ Error al cargar el personaje: ${error.message}`, m);
   }
